f3ae0cd72e2efce769c17c1cb7decebdc5ddb3a5
yasuyuki sato <godasper@gmail.com>
2021-12-27 15:14:09 +0900

    平均価格を取得&ライフサイクルメソッドの確認

--- js/main.js
+++ js/main.js
@@ -2,6 +2,36 @@ const app = new Vue({
   el: "#app",
   data: {
     bpi: null,
+    beforMount: "hoge",
+    mounted: "mounted",
   },
-  methods: {},
+  // 各ライフサイクルのメソッドを実行してデバック
+  // beforeCreate()とmounted()を入れ替えても呼ばれる順番は変わらない
+  beforeCreate() {
+    console.log("beforeCreated");
+  },
+  created() {
+    console.log("Created");
+  },
+
+  // 読み込み時では呼ばれない
+  beforMount() {
+    this.beforMount = "beforeMount";
+    console.log("beforeMount");
+  },
+  // 読み込み時に呼ばれている
+  mounted() {
+    console.log("mounted");
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(function (response) {
+        this.bpi = response.data.bpi;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+
+  // 意図的にコンポーネントが削除される処理をかかないと到達しない
+  beforeDestroy() {},
 });
